a0c7981d9e55f69b1b9cf07a8e377b206c340dbe
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2024-10-07 12:55:08 +0100

    Update backup-router.py

--- backup-router.py
+++ backup-router.py
@@ -1,4 +1,12 @@
+from netmiko import ConnectHandler
 
+#First create the device object using a dictionary
+CSR = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.1.220',
+    'username': 'roger',
+    'password': 'cisco'
+}
 
 # Next establish the SSH connection
 net_connect = ConnectHandler(**CSR)
